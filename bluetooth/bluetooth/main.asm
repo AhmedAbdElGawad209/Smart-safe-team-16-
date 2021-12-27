359f971fac20384377d50b01e5400f36c9ab23b4
Ahmed AdbelGawad <ahmed170137@feng.bu.edu.eg>
2021-12-27 09:06:00 +0200

    bleutooth proteus

--- bluetooth/bluetooth/main.asm
+++ bluetooth/bluetooth/main.asm
@@ -7,6 +7,51 @@
 
 
 ; Replace with your application code
-start:
-    inc r16
-    rjmp start
+.include	"m32def.inc"
+.include  "delay_function.inc"
+
+.cseg
+.org	 0x00
+.equ	baud	= 9600			; baudrate
+	.equ	bps	= 103	; baud prescale
+
+	...
+
+	ldi	r16,LOW(bps)			; load baud prescale
+	ldi	r17,HIGH(bps)			; into r17:r16
+
+	rcall	initUART
+
+	;start:
+	ldi	r16,'a'				; load char 'a' into r16
+	rcall	putc
+	;delay 1000 
+	;rjmp	start
+
+
+	putc:	lds	r17,UCSRA			; load UCSR0A into r17
+	sbrs	r17,UDRE			; wait for empty transmit buffer
+	rjmp	putc				; repeat loop
+
+	sts	UDR,r16			; transmit character
+
+	ret
+
+
+
+
+
+
+
+	initUART:
+	sts	UBRRL,r16			; load baud prescale
+	sts	UBRRH,r17			; to UBRR0
+
+	ldi	r16,(1<<RXEN)|(1<<TXEN)	; enable transmitter
+	sts	UCSRB,r16			; and receiver
+
+	lds r16,UCSRC
+	ori r16,(3<<UCSZ0)
+	sts UCSRC,r16
+
+	ret
