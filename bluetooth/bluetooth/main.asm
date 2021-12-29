93c379b45929764019f777823d093bb4608dc4a9
Aya Mohamed <https://github.com/Aya292Muhammed>
2021-12-29 08:02:28 +0200

    bleutooth update(receiver)

--- bluetooth/bluetooth/main.asm
+++ bluetooth/bluetooth/main.asm
@@ -11,6 +11,7 @@
 .include  "delay_function.inc"
 .include  "initUART.inc"
 .include "send_string.inc"
+.include "receive_char.inc"
 
 .cseg
 .org	 0x00
@@ -20,19 +21,39 @@
 	init_UART
 
 
-	start:
 	
+	/*
 	write myStr
 	
 	delay 1000
 	 
 	 write hello
 	 delay 1000
+	*/
+
+	start:
+
 	
 
+	 
+
+	read r18
+	out DDRC,r18
+	out PORTC,r18
+	read r19
+	out DDRA,r19
+	out PORTA,r19
+
+	
 	rjmp	start
-
-
-
+/*
+	USART_Receive:
+; Wait for data to be received
+sbis UCSRA, RXC
+rjmp USART_Receive
+; Get and return received data from buffer
+in r16, UDR
+ret
+*/
 	myStr:	.db	"elkhazna gahza llsho3'l...!",0x00
 	hello:  .db "ahmed",0x00
